d0b2bce86910ee43009aeaadbf1d5b11d1051497
Andrea Moretti <axyzxp@gmail.com>
2014-07-11 22:43:33 +0200

    -

---
+++ .oh-my-zsh/custom/plugins/axyz/axyz.plugin.zsh
@@ -0,0 +1,52 @@
+alias y='yaourt'
+alias p='pacman'
+alias bu='rsync -av --force --delete'
+alias :q='exit'
+
+function storamount() {
+  sudo mkdir /mnt/stora
+  sudo mount -o "vers=3" -o nolock 192.168.1.2:/home/0common/ /mnt/stora/
+}
+
+function mirrorupdate() {
+  rm /etc/pacman.d/mirrorlist
+  wget -O /etc/pacman.d/_mirrorlist http://www.archlinux.org/mirrorlist/all/
+  sed '/^#\S/ s|#||' -i /etc/pacman.d/_mirrorlist
+  sed 's/$arch/x86_64/g' -i /etc/pacman.d/_mirrorlist
+  rankmirrors -n 10 /etc/pacman.d/_mirrorlist > /etc/pacman.d/mirrorlist
+  rm /etc/pacman.d/_mirrorlist
+}
+
+function randombg() {
+  find ~/.wallpapers/*.* | shuf -n 1 | xargs feh --bg-scale
+}
+
+function eclipse() {
+  cd ~/app/eclipse
+  ./eclipse -vmargs -Xmx2048M
+}
+
+function xinit-ext() {
+  xinit `which $1` -- :1 -xf86config xorg-ext.conf
+}
+
+function xinit-laptop() {
+  xinit `which $1` -- :1 -xf86config xorg-laptop.conf
+}
+
+function xinit-big() {
+  xinit `which $1` -- :1 -xf86config xorg-big.conf
+}
+
+function xinit-big-ru() {
+  xinit `which $1` -- :1 -xf86config xorg-big-ru.conf
+}
+
+function post() {
+  cd /home/axyz/www/axyz.github.io/_posts
+  date=$(date +'%Y-%m-%d')
+  name="$date-$1.markdown"
+  touch $name
+  echo '---\nlayout: post\ntitle: "'$(echo $1 | sed -e "s/-/ /g")'"\ndate: '$date'\ncategories: '$(echo $2 | sed -e "s/,/ /g")'\n---\n\n' > $name 
+  vim $name
+} 
